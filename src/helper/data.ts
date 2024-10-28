566e066ffd1b12fac05627b09c282afc7e6e6112
thanhtran1987 <42910048+thanhtran1987@users.noreply.github.com>
2024-10-28 20:19:35 +1100

    asd

--- src/helper/data.ts
+++ src/helper/data.ts
@@ -1,6 +1,6 @@
 export async function getData(session: string) {
   console.log(111, process.env.VERCEL_URL);
-  let url = `http://${process.env.VERCEL_URL}/api/task`;
+  let url = `https://${process.env.VERCEL_URL}/api/task`;
   console.log(112, url);
   const res = await fetch(url, {
     method: "GET",
@@ -14,7 +14,7 @@ export async function getData(session: string) {
 }
 export async function getConfig() {
   console.log(114);
-  let url = `http://${process.env.VERCEL_URL}/config.json`;
+  let url = `https://${process.env.VERCEL_URL}/config.json`;
   console.log(112, url);
   const res = await fetch(url);
   return res.json();
